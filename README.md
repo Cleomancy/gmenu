e1377460a951af7eab26b5ef1d3ca40aa1a22a2a
Cleomancy <77933444+Cleomancy@users.noreply.github.com>
2025-01-04 01:51:26 +0100

    updated dmenu to version 5.3 with more patches

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
 # Glorious Dmenu
 
 ## Rice:
-I use DWM with slstatus, st is my terminal and dmenu is my god.
+I use DWM with dwmblocks, st is my terminal and dmenu is my god.
 This is my build of dmenu with mouse support included.
 
 ## Prerequisites:
 I use arch btw,
 ```
-pacman -Sy base-devel xorg-server xorg-xinit libxinerama libxft feh unclutter ttf-font-awesome ttf-hack
+pacman -Sy base-devel xorg-server xorg-xinit libxinerama
 ```
-You can already guess my dotfiles.
 
 ## Patches:
 
 - [mouse support](https://tools.suckless.org/dmenu/patches/mouse-support/)
 - [highlight](https://tools.suckless.org/dmenu/patches/highlight/)
+- [border](https://tools.suckless.org/dmenu/patches/border/)
+- [lines below prompt](https://tools.suckless.org/dmenu/patches/lines-below-prompt/)
 
 ## Install:
 ```
--- config.h
+++ config.h
@@ -4,15 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"hack:size=18"
+	"monospace:size=18"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#77006D" },
-	[SchemeSelHighlight] = { "#E660E0", "#3C1857" },
-	[SchemeNormHighlight] = { "#E660E0", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#3C1857" },
+	[SchemeSelHighlight] = { "#ffc978", "#3C1857" },
+	[SchemeNormHighlight] = { "#ffc978", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
@@ -23,3 +23,6 @@ static unsigned int lines      = 0;
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static unsigned int border_width = 5;
